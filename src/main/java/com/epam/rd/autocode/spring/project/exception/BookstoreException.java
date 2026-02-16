6cb85196f4d8bbe39a44ea2f92a6abb04ddf71aa
AlexMkr <a.mkrtichiani7@gmail.com>
2026-02-16 05:47:43 +0400

    Reverting to N1 state to fix build errors

--- src/main/java/com/epam/rd/autocode/spring/project/exception/BookstoreException.java
+++ src/main/java/com/epam/rd/autocode/spring/project/exception/BookstoreException.java
@@ -1,4 +1,7 @@
 package com.epam.rd.autocode.spring.project.exception;
 
-public class BookstoreException {
+public class BookstoreException extends RuntimeException {
+    public BookstoreException(String message) {
+        super(message);
+    }
 }
